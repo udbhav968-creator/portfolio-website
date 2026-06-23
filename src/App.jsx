59128711fe804c305e91c94e8ab16a7ccaeab952
udbhav968-creator <snojkumar968@gmail.com>
2026-06-23 23:19:39 +0530

    feat: Create core App component structure

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import About from './components/About';
+import Experience from './components/Experience';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <Navbar />
+      <main>
+        <Hero />
+        <About />
+        <Experience />
+        <Projects />
+        <Contact />
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
